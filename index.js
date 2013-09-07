4073afb734325af62a67c7d6c95dfef627d84b3e
Sahar Kazemzadeh <kazemzad@seas.1899.apng.wireless-pennnet.upenn.edu>
2013-09-07 15:38:56 -0400

    Added Highcharts js

--- index.js
+++ index.js
@@ -1,14 +1,67 @@
- $(document).ready(function() {
+$(document).ready(function() {
+    var optionsLine = {
+        chart: {
+            renderTo: 'linecontainer',
+            type: 'line',
+            marginRight: 130,
+            marginBottom: 25
+        },
+        title: {
+            text: 'Billing History',
+                    x: -20 //center
+                },
+                subtitle: {
+                    text: '',
+                    x: -20
+                },
+                xAxis: {
+                    categories: []
+                },
+                yAxis: {
+                    title: {
+                        text: 'Amount'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function() {
+                        return '<b>'+ this.series.name +'</b><br/>'+
+                        this.x +': '+ this.y;
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'top',
+                    x: -10,
+                    y: 100,
+                    borderWidth: 0
+                },
+                series: []
+            }
+            
+            $.getJSON("data.json", function(json) {
+                optionsLine.xAxis.categories = json[0]['data'];
+                optionsLine.series[0] = json[1];
+                optionsLine.series[1] = json[2];
+                optionsLine.series[2] = json[3];
+                optionsLine.series[3] = json[4];
+                chart = new Highcharts.Chart(optionsLine);
+            }).error(function() {console.log('error');});
 
-            var options = {
+             var optionsPie = {
                 chart: {
-                    renderTo: 'linecontainer',
-                    type: 'line',
+                    renderTo: 'piecontainer',
+                    type: 'pie',
                     marginRight: 130,
                     marginBottom: 25
                 },
                 title: {
-                    text: 'Billing History',
+                    text: 'Billing by Categories',
                     x: -20 //center
                 },
                 subtitle: {
@@ -42,15 +95,16 @@
                     y: 100,
                     borderWidth: 0
                 },
-                series: []
-            }
-            
-            $.getJSON("data.json", function(json) {
-                options.xAxis.categories = json[0]['data'];
-                options.series[0] = json[1];
-                options.series[1] = json[2];
-                options.series[2] = json[3];
-                options.series[3] = json[4];
-                chart = new Highcharts.Chart(options);
-            }).error(function() {console.log('error');});
-        });
+                series: [{
+                    type: 'pie',
+                    name: 'Browser share', 
+                    data: []    
+                }]
+            }           
+            $.getJSON("data2.json", function(json) {
+                optionsPie.series = json;
+                 chartPie = new Highcharts.Chart(optionsPie);
+            });
+
+
+});
