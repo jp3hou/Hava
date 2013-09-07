6bd24301ce478eb0bf34c4227ab5c8cf9de16089
Sahar Kazemzadeh <kazemzad@seas.1899.apng.wireless-pennnet.upenn.edu>
2013-09-07 13:48:33 -0400

    Added Highcharts js

---
+++ index.js
@@ -0,0 +1,56 @@
+ $(document).ready(function() {
+
+            var options = {
+                chart: {
+                    renderTo: 'linecontainer',
+                    type: 'line',
+                    marginRight: 130,
+                    marginBottom: 25
+                },
+                title: {
+                    text: 'Billing History',
+                    x: -20 //center
+                },
+                subtitle: {
+                    text: '',
+                    x: -20
+                },
+                xAxis: {
+                    categories: []
+                },
+                yAxis: {
+                    title: {
+                        text: 'Amount'
+                    },
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                },
+                tooltip: {
+                    formatter: function() {
+                            return '<b>'+ this.series.name +'</b><br/>'+
+                            this.x +': '+ this.y;
+                    }
+                },
+                legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'top',
+                    x: -10,
+                    y: 100,
+                    borderWidth: 0
+                },
+                series: []
+            }
+            
+            $.getJSON("data.json", function(json) {
+                options.xAxis.categories = json[0]['data'];
+                options.series[0] = json[1];
+                options.series[1] = json[2];
+                options.series[2] = json[3];
+                options.series[3] = json[4];
+                chart = new Highcharts.Chart(options);
+            }).error(function() {console.log('error');});
+        });
