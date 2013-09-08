2e78520083e6a1a1015b4f80ee2803b684608977
Julia Chou <jpchou@ad.unc.edu>
2013-09-08 05:29:03 -0400

    bills changes

--- jquery-styling.js
+++ jquery-styling.js
@@ -1,14 +1,18 @@
+var monthnumber;
 function go() {
 	var waterselector = $("#water");
 	var gasselector = $("#gas");
 	var electricityselector = $("#electricity");
 	var internetselector = $("#internet");
+	var monthselector = $(".months");
+	var yearselector = $(".year");
 
+	var monthn = monthselector.val();
+	var year = yearselector.val();
     	var waterprice = waterselector.val();
 	var gasprice = gasselector.val();
     	var electricityprice = electricityselector.val();
 	var internetprice = internetselector.val();
-	//alert(waterprice + "," + gasprice + "," + electricityprice + "," + internetprice);
 	if (waterprice !== "" && gasprice !== "" && electricityprice !== "" && internetprice !== ""){
 		try{
 			var totalprice = Number(waterprice) + Number(gasprice) + Number(electricityprice) + Number(internetprice);
@@ -25,7 +29,7 @@ function go() {
 		internetselector.val("");
 		}
 		
-	}	
+}	
 
 
 
