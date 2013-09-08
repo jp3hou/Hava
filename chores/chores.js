95867654a5df8369d8dada4828dae74b5f60caaa
Roxanne <roxestradah@gmail.com>
2013-09-08 09:31:01 -0400

    chores changes

--- chores/chores.js
+++ chores/chores.js
@@ -1,30 +1,34 @@
-    function go()
-    {
-      alert("header1");
-    }
-
-    function displayChores(areaName){
+function displayChores(areaName){
       var chores = ['mop/sweep', 'trash', 'dishes', 'vacuuming'];
       var housemates = ['Sahar', 'Roxanne', 'Julia', 'Nicole'];
       var currentDate = Date.now(); // corresponds to current time
       var startDate = new Date();
-      startDate.setDate(1);
-      var elapsedWeeks = Math.floor((currentDate - startDate) / 604800000);
-
+      startDate.setDate(1); // sets the current date ie. 1 sets date to 1 but all else not mutated
+      startDate.setMonth(7); // sets the month.
+      var elapsedWeeks = Math.floor((currentDate - startDate) / 604800000); // divides milliseconds passed by week
       var object = document.getElementById(areaName);
       object.innerHTML += '';
-
       for (var i = 0; i < chores.length; i++) {
         
         var matchingIndex = (i + elapsedWeeks);
         
+        // make chores loop around if weeks passed number of chores
         if (matchingIndex >= chores.length) {
-          while ((matchingIndex >= chores.length) && ((matchingIndex-i) >=  0))
+          while ((matchingIndex >= chores.length) && ((matchingIndex-chores.length) >=  0))
           {
-            matchingIndex = (matchingIndex - i);
+            matchingIndex = (matchingIndex - chores.length);
           }
         }
     
-        object.innerHTML += '<li>' + housemates[matchingIndex] + ' is assigned ' + chores[matchingIndex] + ' this week.</li>';
+        // print the statement
+        object.innerHTML += '<li>' + housemates[i] + ' is assigned ' + chores[matchingIndex] + ' this week.</li>';
+        if ((matchingIndex + 1) < chores.length)
+        {
+          object.innerHTML += '<li>' + ' Next week ' + housemates[i] + ' has ' + chores[matchingIndex+1] + '</li>';   
+        }
+        else
+        {
+          object.innerHTML += '<li>' +x ' Next week ' + housemates[i] + ' has ' + chores[0] + '</li>';
+        }
       }
     }
