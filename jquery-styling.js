1504a18b81495990fbdd34a81d9ab119a8e9dc69
Julia Chou <jpchou@ad.unc.edu>
2013-09-08 00:20:06 -0400

    fixing bills

--- jquery-styling.js
+++ jquery-styling.js
@@ -1,15 +1,31 @@
 function go() {
-	var descriptionselector = $("#description");
-	var priceselector = $("#money-amount");
-    	var description = descriptionselector.val();
-	var price = priceselector.val();
-	if (description !== "" && price !== ""){
-		var newtablerow = "<tr> <td align=\"left\"><p>" + description + ": "+ price + "</p>" + "</td></tr>";
+	var waterselector = $("#water");
+	var gasselector = $("#gas");
+	var electricityselector = $("#electricity");
+	var internetselector = $("#internet");
+
+    	var waterprice = waterselector.val();
+	var gasprice = gasselector.val();
+    	var electricityprice = electricityselector.val();
+	var internetprice = internetselector.val();
+	//alert(waterprice + "," + gasprice + "," + electricityprice + "," + internetprice);
+	if (waterprice !== "" && gasprice !== "" && electricityprice !== "" && internetprice !== ""){
+		try{
+			var totalprice = Number(waterprice) + Number(gasprice) + Number(electricityprice) + Number(internetprice);
+		}
+		catch(err){
+			alert("Input invalid");		
+		}
+		var newtablerow = "<tr> <td align=\"left\"><p> Total: $" + totalprice + "</p>" + "</td></tr>";
 		var tableselector = $(".utilities tr:last");
 		tableselector.after(newtablerow);
-		priceselector.val("");
-		descriptionselector.val("");
+		waterselector.val("");
+		gasselector.val("");
+		electricityselector.val("");
+		internetselector.val("");
+		}
+		
 	}	
-}
+
 
 
